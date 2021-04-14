d822246cdc54e745db29134fdabbd27dfc3dc0da
Dev E.B <81972855+Dev-EB@users.noreply.github.com>
2021-04-14 19:45:28 +0900

    Create Selection.cpp

---
+++ Selection/Selection.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int& a, int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+//Main Algorithm
+void SelectionSort(int iArray[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {          
+        int temp = size - 1;
+        for (int j = 0 + i; j < size; j++)
+        {
+            if (iArray[j] <= iArray[temp])
+            {
+                temp = j;
+            }
+        }
+        Swap(iArray[i], iArray[temp]);
+    }
+}
+
+int main()
+{
+    int iArray[10] = { 1,5,9,3,2,4,7,8,6,0 };
+    SelectionSort(iArray, sizeof(iArray) / sizeof(int));
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << iArray[i];
+    }
+}

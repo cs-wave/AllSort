7037e693a6a206577476c1e792a3310588400460
acceler8tion <rurchi1206@gmail.com>
2021-04-15 20:46:45 +0900

    remove JSDoc

--- Bubble/Bubble.js
+++ Bubble/Bubble.js
@@ -1,8 +1,3 @@
-/**
- * 버블정렬 수행
- * 
- * @param {Array<Number>} arr 정렬할 배열 
- */
 function bubbleSort(arr) {
     for(let i = 0; i < arr.length-1; i++) {
         for(let j = 0; j < arr.length-i-1; j++) {
@@ -13,13 +8,6 @@ function bubbleSort(arr) {
     }
 }
 
-/**
- * arr의 idx1과 idx2의 인덱스에 해당하는 두 원소 서로 교체
- * 
- * @param {Array<Number>} arr 스왑할 배열
- * @param {Number} idx1 1번째 인덱스
- * @param {Number} idx2 2번째 인덱스
- */
 function swap(arr, idx1, idx2) {
     let tmp = arr[idx2];
     arr[idx2] = arr[idx1];

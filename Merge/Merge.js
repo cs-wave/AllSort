a8a17dfebcb32c8af85b59dc63b81d56381b5260
acceler8tion <rurchi1206@gmail.com>
2021-04-16 19:12:12 +0900

    인텐트 간격 수정 2->4

--- Merge/Merge.js
+++ Merge/Merge.js
@@ -1,36 +1,36 @@
 function mergeSort(arr, start, end) {
-  if(end-start > 0) {
-    let mid = Math.floor((end+start)/2)+1;
-    mergeSort(arr, start, mid-1);
-    mergeSort(arr, mid, end);
-    merge(arr, start, mid, end);  
-  }
+    if(end-start > 0) {
+        let mid = Math.floor((end+start)/2)+1;
+        mergeSort(arr, start, mid-1);
+        mergeSort(arr, mid, end);
+        merge(arr, start, mid, end);  
+    }
 }
 
 function merge(arr, start, mid, end) {
-  let map = [];
-  let i, j, l;
-  i = start, j = mid;
-  l = end-start+1;
-  while(i < mid && j < end+1) {
-    if(arr[i] > arr[j]) {
-      map.push(arr[j++]);
-    } else {
-      map.push(arr[i++]);
+    let map = [];
+    let i, j, l;
+    i = start, j = mid;
+    l = end-start+1;
+    while(i < mid && j < end+1) {
+        if(arr[i] > arr[j]) {
+            map.push(arr[j++]);
+        } else {
+            map.push(arr[i++]);
+        }
     }
-  }
   
-  while(i < mid) {
-    map.push(arr[i++]);
-  }
+    while(i < mid) {
+        map.push(arr[i++]);
+    }
   
-  while(j < end+1) {
-    map.push(arr[j++]);
-  }
+    while(j < end+1) {
+        map.push(arr[j++]);
+    }
   
-  for(let k = 0; k < l; k++) {
-    arr[k+start] = map[k];
-  }
+    for(let k = 0; k < l; k++) {
+        arr[k+start] = map[k];
+    }
 }
 
 let arr= [];

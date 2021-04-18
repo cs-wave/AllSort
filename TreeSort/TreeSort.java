48b495f197bfe67d8a5c8996302fac6ba98f11cd
acceler8tion <rurchi1206@gmail.com>
2021-04-19 00:11:53 +0900

    tree-sort java구현체 추가

---
+++ TreeSort/TreeSort.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class TreeSort {
+    private static final int ARRAY_SIZE = 1000;
+    private static int index = 0;
+
+    private static class BinaryTree {
+        int value;
+        BinaryTree left = null;
+        BinaryTree right = null;
+
+        BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[ARRAY_SIZE];
+        for(int i = 0; i < ARRAY_SIZE; i++) {
+            arr[i] = i;
+        }
+        Collections.shuffle(Arrays.asList(arr));
+        int[] newArr = Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+        treeSort(newArr, ARRAY_SIZE);
+
+        for(int i : newArr) {
+            System.out.println(i);
+        }
+    }
+
+    public static void treeSort(int[] arr, int len) {
+        if(len  < 1) return;
+
+        BinaryTree root = new BinaryTree(arr[0]);
+        for(int i = 1; i < len; i++) {
+            root = insert(root, arr[i]);
+        }
+        inOrder(root, arr);
+        index = 0;
+    }
+
+    public static BinaryTree insert(BinaryTree t, int value) {
+        if(t == null) {
+            return new BinaryTree(value);
+        } else {
+            if(value > t.value) {
+                t.right = insert(t.right, value);
+            } else {
+                t.left = insert(t.left, value);
+            }
+            return t;
+        }
+    }
+
+    public static void inOrder(BinaryTree t, int[] arr) {
+        if(t != null) {
+            inOrder(t.left, arr);
+            arr[index++] = t.value;
+            inOrder(t.right, arr);
+        }
+    }
+}
